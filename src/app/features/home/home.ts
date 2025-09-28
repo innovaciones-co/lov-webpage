4210527fab7df1c030a6598de3c3e26489e31dea
Sergio Torres <sdtorresl@unal.edu.co>
2025-09-27 19:14:13 -0500

    feat: refactor home component structure and add intro component

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { Promo } from "../promo/promo";
+import { Promo } from "../../shared/components/promo/promo";
+import { Intro } from "../intro/intro";
 
 @Component({
   selector: 'app-home',
-  imports: [Promo],
+  imports: [Promo, Intro],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
