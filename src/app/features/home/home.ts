091abd31d400114d86f17e32ccc0dd65e4a25f65
Sergio Torres <sdtorresl@unal.edu.co>
2025-09-28 23:30:26 -0500

    feat: add legals component with legal documents and styling; update home layout to include legals section

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Promo } from "../../shared/components/promo/promo";
 import { Intro } from "../intro/intro";
 import { IntroPortability } from "../intro-portability/intro-portability";
+import { Legals } from "../legals/legals";
 
 @Component({
   selector: 'app-home',
-  imports: [Promo, Intro, IntroPortability],
+  imports: [Promo, Intro, IntroPortability, Legals],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
