43a16d03b1c25ad001a031e36cd65b48b422f6f4
Sara Sanchez <saralesan@hotmail.com>
2025-09-29 18:15:35 -0500

    feat: create plan component with base card styles

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { Promo } from "../../shared/components/promo/promo";
-import { Intro } from "../intro/intro";
-import { IntroPortability } from "../intro-portability/intro-portability";
+import { Promo } from '../../shared/components/promo/promo';
+import { Intro } from '../intro/intro';
+import { IntroPortability } from '../intro-portability/intro-portability';
+import { Plans } from '../plans/plans';
 
 @Component({
   selector: 'app-home',
-  imports: [Promo, Intro, IntroPortability],
+  imports: [Promo, Intro, IntroPortability, Plans],
   templateUrl: './home.html',
-  styleUrl: './home.scss'
+  styleUrl: './home.scss',
 })
-export class Home {
-
-}
+export class Home {}
