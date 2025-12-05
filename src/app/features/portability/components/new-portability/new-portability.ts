f265ceaeb057ba13c47a2137e597613cb1f48552
saralesan <saralesan@hotmail.com>
2025-12-04 20:54:37 -0500

    refactor(portability): simplify step navigation and remove unused step logic

--- src/app/features/portability/components/new-portability/new-portability.ts
+++ src/app/features/portability/components/new-portability/new-portability.ts
@@ -1,15 +1,9 @@
-import { Component, signal, computed, Type, inject } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../../environments/environment';
 import { PortabilityInformation, PortabilityInformationData } from '../portability-information-form/portability-information-form.component';
 import { PortinInformationFormComponent, PortinInformationData } from '../portin-information-form/portin-information-form.component';
 import { CustomerInformationFormComponent, CustomerInformationData } from '../customer-information-form/customer-information-form';
-
-interface Step {
-  label: string;
-  component: Type<any>;
-  completed?: boolean;
-}
 
 @Component({
   selector: 'app-new-portability',
@@ -25,39 +19,10 @@ export class NewPortabilityComponent {
   portinData = signal<PortinInformationData | null>(null);
   customerData = signal<CustomerInformationData | null>(null);
 
-  steps: Step[] = [
-    { label: 'Info Portabilidad', component: PortabilityInformation, completed: false },
-    { label: 'Formulario Portabilidad', component: PortinInformationFormComponent, completed: false },
-    { label: 'Info cliente', component: CustomerInformationFormComponent, completed: false },
-  ];
-
   currentStepIndex = signal(0);
 
-  currentStep = computed(() => this.steps[this.currentStepIndex()]);
-
-  isStepCompleted(stepIndex: number): boolean {
-    return this.steps[stepIndex].completed || false;
-  }
-
-  isStepActive(stepIndex: number): boolean {
-    return this.currentStepIndex() === stepIndex;
-  }
-
-  canNavigateToStep(stepIndex: number): boolean {
-    // Can navigate to current step, next step, or any completed step
-    return stepIndex <= this.currentStepIndex() + 1 || this.isStepCompleted(stepIndex);
-  }
-
-  selectStep(stepIndex: number): void {
-    if (this.canNavigateToStep(stepIndex)) {
-      this.currentStepIndex.set(stepIndex);
-    }
-  }
-
   nextStep(): void {
-    if (this.currentStepIndex() < this.steps.length - 1) {
-      // Mark current step as completed when moving to next
-      this.steps[this.currentStepIndex()].completed = true;
+    if (this.currentStepIndex() < 2) { // 0, 1, 2 (3 steps total)
       this.currentStepIndex.set(this.currentStepIndex() + 1);
     }
   }
@@ -77,8 +42,9 @@ export class NewPortabilityComponent {
   onPortinInformationSubmit(data: PortinInformationData): void {
     this.portinData.set(data);
     console.log('Portin Information Data:', data);
+    this.nextStep();
 
-    // Make GET request to lookup donorNumber
+    /* // Make GET request to lookup donorNumber
     const url = `${environment.apiUrl}/mnp/lookup/${data.donorNumber}`;
     console.log('Making GET request to:', url);
 
@@ -92,7 +58,7 @@ export class NewPortabilityComponent {
         // For now, no proceeding to next step
         // this.nextStep();
       }
-    });
+    }); */
   }
 
   onCustomerInformationSubmit(data: CustomerInformationData): void {
