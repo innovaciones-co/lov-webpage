a2ccb8affe03806de3ea6ac82d4a3a3aa88f3e22
Sergio Torres <sdtorresl@unal.edu.co>
2025-12-11 17:58:59 -0500

    feat(portability): add staging configuration and update gateway URL for environments

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     apiUrl: 'https://lov-webservices-dev.innovaciones.co/api',
-    gatewayUrl: 'https://lov-gateway.innovaciones.co'
+    gatewayUrl: 'http://lov-gateway.innovaciones.co'
 };
