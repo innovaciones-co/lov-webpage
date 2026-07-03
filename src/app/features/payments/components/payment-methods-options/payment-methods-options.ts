8a08ad819fc1299a6e1a7119ab82bbbe76ff52ac
saralesan <saralesan@hotmail.com>
2026-07-02 23:06:32 -0500

    feat: add PaymentMethodOptions component

---
+++ src/app/features/payments/components/payment-methods-options/payment-methods-options.ts
@@ -0,0 +1,42 @@
+import { Component, ChangeDetectionStrategy, signal, ViewChild, TemplateRef, AfterViewInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RadioComponent } from '../../../../shared/components/form-fields/radio/radio';
+
+@Component({
+  selector: 'app-payment-method-options',
+  imports: [RadioComponent, ReactiveFormsModule, CommonModule],
+  templateUrl: './payment-methods-options.html',
+  styleUrl: './payment-methods-options.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PaymentMethodOptions implements AfterViewInit {
+  @ViewChild('recurringTemplate') recurringTemplate?: TemplateRef<any>;
+  @ViewChild('balanceTemplate') balanceTemplate?: TemplateRef<any>;
+  @ViewChild('payuTemplate') payuTemplate?: TemplateRef<any>;
+
+  paymentMethodControl = new FormControl('');
+
+  paymentMethods = signal<any[]>([]);
+
+  ngAfterViewInit() {
+    this.paymentMethods.set([
+      {
+        value: 'recurring',
+        template: this.recurringTemplate
+      },
+      {
+        value: 'balance',
+        template: this.balanceTemplate
+      },
+      {
+        value: 'payu',
+        template: this.payuTemplate
+      }
+    ]);
+  }
+
+  onPaymentMethodChange(method: string): void {
+    console.log('Método de pago seleccionado:', method);
+  }
+}
